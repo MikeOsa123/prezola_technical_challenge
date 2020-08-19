46f251874d1a51324aec2dbf2d8afec0ab0ebea6
MikeOsa123 <m.osamwonyi@gmail.com>
2020-08-19 18:23:22 +0100

    created home template and navbars

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Michael'}
+    return render_template('index.html', title='Home', user=user)
